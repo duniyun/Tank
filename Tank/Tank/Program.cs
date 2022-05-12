a5ee0b5a3a7796ef92422526a08fa2c7d5718927
duniyun <88871965+duniyun@users.noreply.github.com>
2022-05-12 17:06:32 +0800

    修改17.17

--- Tank/Tank/Program.cs
+++ Tank/Tank/Program.cs
@@ -9,7 +9,7 @@ namespace Tank
       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("duniyun!");
         }
         
     }
