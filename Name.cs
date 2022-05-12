6d75e10dee40002e013322ff7fa808c246e35e1a
独逆云 <1101070822@qq.com>
2022-05-12 16:21:24 +0800

    test

---
+++ Name.cs
@@ -0,0 +1,14 @@
+public class Dss {
+    int xx;
+    int ss;
+    public int Setsss(int x,int y){
+        int temp;
+        temp=x;
+        x=y;
+        return 1;
+    }
+    public void Sss(){
+        Setsss(1,2);
+    }
+    int s=Setsss(1,1);
+}
---
+++ Tank/Tank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tank
+{
+    internal class Program
+    {
+        int s = 1;
+        int t = 2;
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+    }
+}
